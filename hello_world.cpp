17c558241a7e080997e02930b034591cfba95824
Mager2112 <kostya.pikulin@yandex.ru>
2022-04-21 20:29:46 +0300

    udded comments to  hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main()
 {
-	std::string name;
-	std::cin >> name;
-        std::cout<< "Hello, world! from" << name << endl;
+	std::string name; //Имя пользователя
+	std::cin >> name; //Ввод
+        std::cout<< "Hello, world! from" << name << endl; //Вывод
 }
